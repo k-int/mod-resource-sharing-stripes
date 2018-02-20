8e8bcd167df4a3149c52dd909ed5f4969f6cccd5
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2018-02-20 16:27:40 +0000

    Correct the error with calling cd twice.

--- postinstall.sh
+++ postinstall.sh
@@ -1,2 +1,2 @@
-[ ! -e 'dist/app-resource-sharing' ] || (cd dist/app-resource-sharing && cd dist/app-resource-sharing && yarn link && cd ../ && yarn link @k-int/resource-sharing)
+[ ! -e 'dist/app-resource-sharing' ] || (cd 'dist/app-resource-sharing' && yarn link && cd ../ && yarn link @k-int/resource-sharing)
 # cd plugin-schema-forms && yarn link && cd ../ && yarn link @folio/plugin-schema-forms
