a5a942d86e83a6a113a1ca69402e9c81eec032c0
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2018-02-20 16:21:03 +0000

    Check for dir first.

--- postinstall.sh
+++ postinstall.sh
@@ -1,2 +1,2 @@
- cd dist/app-resource-sharing && yarn link && cd ../ && yarn link @k-int/resource-sharing
+[ -d foo ] && cd dist/app-resource-sharing && yarn link && cd ../ && yarn link @k-int/resource-sharing
 # cd plugin-schema-forms && yarn link && cd ../ && yarn link @folio/plugin-schema-forms
